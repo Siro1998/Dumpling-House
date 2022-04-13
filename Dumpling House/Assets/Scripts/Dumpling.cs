6e125d0eda51d75a290918d31daea6bcf6e6f937
Siro1998 <zengxiangrui1998@gmail.com>
2022-04-13 16:52:30 -0400

    Creating Dumpling Prefab
    Check the GameScene to see dumplings explode

---
+++ Dumpling House/Assets/Scripts/Dumpling.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dumpling : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+    public LayerMask levelMask;
+    private bool exploded = false;
+    void Start()
+    {
+        Invoke("Explode", 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Explode()
+    {
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        StartCoroutine(CreateExplosions(Vector3.up));
+        StartCoroutine(CreateExplosions(Vector3.right));
+        StartCoroutine(CreateExplosions(Vector3.down));
+        StartCoroutine(CreateExplosions(Vector3.left)); 
+        
+        GetComponent<SpriteRenderer>().enabled = false;
+        exploded = true;
+        GetComponent<CircleCollider2D>().enabled = false;
+        //transform.Find("Collider").gameObject.SetActive(false);
+        Destroy(gameObject, .3f);
+    }
+    
+    private IEnumerator CreateExplosions(Vector3 direction)
+    {
+        for (int i = 1; i < 3; i++) 
+        { 
+            RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(0,0,0.5f), direction, i, levelMask);
+            //Physics.Raycast(transform.position + new Vector3(0,.5f,0), direction, out hit, i, levelMask);
+            if (!hit.collider) 
+            { 
+                Instantiate(explosionPrefab, transform.position + (i * direction),explosionPrefab.transform.rotation);
+            } 
+            else 
+            {
+            break; 
+            }
+            yield return new WaitForSeconds(.05f); 
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (!exploded && other.CompareTag("Explosion"))
+        {
+            CancelInvoke("Explode");
+            Explode(); // 3
+        }
+    }
+}
